8dd28ac5870021a51f861e7901b758bf6bb292b1
Wesley Werikis <wesleywerikis@outlook.com>
2026-02-20 23:53:38 -0300

    test: add Spring Boot context smoke test

---
+++ src/test/java/br/com/wesleysilva/cfsat/CfsatImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wesleysilva.cfsat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfsatImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
